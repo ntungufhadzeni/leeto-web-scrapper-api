6a90f3fa6cacd688af3e1cb2828361da1cba03ab
ntungufhadzeni <mbudzeni@mrhconsult.co.za>
2022-11-23 00:46:07 +0200

    sort data using pandas

--- leeto_web_scrapper.py
+++ leeto_web_scrapper.py
@@ -6,12 +6,15 @@ import os
 import datetime
 from models import Notice, Base
 from config import engine
+import pandas as pd
 
 url = "https://leetolapolokwane.co.za/latest-news/"
 
-Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
 Session = sessionmaker()
 db = Session(bind=engine)
+
+data = []
 
 
 def scrap_website():
@@ -22,11 +25,32 @@ def scrap_website():
     posts = soup.find_all('li', class_='clr')
 
     for post in posts:
+        row = {}
         title = post.a['title']
         link = post.a['href']
         thumbnail = post.img['src']
         date = post.find('div', class_='recent-posts-date').text.strip()[:-1]
+        row['title'] = title
+        row['link'] = link
+        row['thumbnail'] = thumbnail
+        row['date'] = date
 
+        data.append(row)
+
+
+def sort_data():
+    arg_df = pd.DataFrame(data)
+    arg_df['DT'] = pd.to_datetime(arg_df.date)
+    arg_df.sort_values(by='DT').reset_index(inplace=True)
+    return arg_df
+
+
+def add_to_database(arg_df):
+    for index in arg_df.index:
+        date = arg_df.loc[index, 'date']
+        link = arg_df.loc[index, 'link']
+        title = arg_df.loc[index, 'title']
+        thumbnail = arg_df.loc[index, 'thumbnail']
         try:
             new_notice = Notice(date, title, link, thumbnail)
             db.add(new_notice)
@@ -37,3 +61,5 @@ def scrap_website():
 
 if __name__ == '__main__':
     scrap_website()
+    df = sort_data()
+    add_to_database(df)
